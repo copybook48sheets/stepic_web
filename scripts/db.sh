496824266d5437fa9e1280a0baa087af4e7db88d
copybook48sheets <disabled1234@mail.ru>
2016-03-13 20:02:53 +0300

    Update db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 sudo /etc/init.d/mysql restart
-mysqld &
-
-mysql -uroot -e "create database if not exists ask_db"
-mysql -uroot -e "grant all on ask_db.* to 'ask_user'@'localhost' identified by 'ask_pwd'"
-
-cd ../ask
-python manage.py syncdb
+mysql -uroot -e "CREATE DATABASE ask_db;"
+mysql -uroot -e "CREATE USER 'ask_user'@'localhost' IDENTIFIED BY 'ask_pwd';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ask_db.* TO 'ask_user'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
