0a06295b42b78fc4d661292a709dedf367191f5c
copybook48sheets <disabled1234@mail.ru>
2016-03-14 20:35:00 +0300

    1

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -11,13 +11,13 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
 try:
-    from stepic_web.ask.qa.models import Question, Answer
-    from stepic_web.ask.qa.forms import AskForm, AnswerForm, SignupForm, LoginForm
+    from qa.models import Question, Answer
+    from qa.forms import AskForm, AnswerForm, SignupForm, LoginForm
 except ImportError:
     import sys
     sys.path.append("/home/box")
-    from web.ask.qa.models import Question, Answer
-    from web.ask.qa.forms import AskForm, AnswerForm, SignupForm, LoginForm
+    from qa.models import Question, Answer
+    from qa.forms import AskForm, AnswerForm, SignupForm, LoginForm
 
 LIMIT = 10
 
