2adb6b6a466e765a7e282f18cec3fde93ef607f5
copybook48sheets <disabled1234@mail.ru>
2016-03-14 20:33:41 +0300

    1

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -8,11 +8,11 @@ from django import forms
 from django.contrib.auth.models import User
 
 try:
-    from stepic_web.ask.qa.models import Question, Answer
+    from qa.models import Question, Answer
 except ImportError:
     import sys
     sys.path.append("/home/box")
-    from web.ask.qa.models import Question, Answer
+    from qa.models import Question, Answer
 
 
 class AskForm(forms.Form):
