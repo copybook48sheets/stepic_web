ea0127933d1fe427a23a2b52b9acab30d8d69a96
copybook48sheets <disabled1234@mail.ru>
2016-03-14 20:26:15 +0300

    1

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,11 +2,9 @@
 
 sudo rm /etc/nginx/sites-enabled/default
 
-cd ..
 sudo ln -s etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-gunicorn -c etc/gunicorn_conf.py hello:application &
+gunicorn -c /home/box/web/etc/gunicorn_conf.py hello:application &
 
-cd ask
-gunicorn -c ../etc/gunicorn_django_conf.py ask.wsgi --pythonpath '/home/box/web/ask' &
+gunicorn -c /home/box/web/etc/gunicorn_django_conf.py ask.wsgi --pythonpath '/home/box/web/ask' &
