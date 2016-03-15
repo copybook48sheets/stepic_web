47a6583677fc4bc33a8cbdd42d686c599942d1c6
copybook48sheets <disabled1234@mail.ru>
2016-03-15 19:08:04 +0300

    1
    1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -52,6 +52,7 @@ ROOT_URLCONF = 'ask.urls'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.core.context_processors.csrf',
