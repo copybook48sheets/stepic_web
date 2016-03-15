e29492b439ebc2bca4ad0a9a4784d89f64969a22
copybook48sheets <disabled1234@mail.ru>
2016-03-15 18:20:42 +0300

    1
    1

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 
 """
-
 """
 
 from django import forms
-from django.contrib.auth.models import User
 
 try:
-    from qa.models import Question, Answer
+    from stepic_web.ask.qa.models import Question, Answer
 except ImportError:
     import sys
     sys.path.append("/home/box")
-    from qa.models import Question, Answer
+    from web.ask.qa.models import Question, Answer
 
 
 class AskForm(forms.Form):
     title = forms.CharField(min_length=1)
     text = forms.CharField(min_length=1, widget=forms.Textarea)
 
-    def __init__(self, user=None, **kwargs):
+    def __init__(self, user, **kwargs):
         self.user = user
-        super(AskForm, self).__init__(kwargs)
+        super().__init__(kwargs)
 
     def save(self):
         self.cleaned_data["author"] = self.user
@@ -34,9 +32,9 @@ class AnswerForm(forms.Form):
     text = forms.CharField(min_length=1, widget=forms.Textarea)
     question = forms.IntegerField()
 
-    def __init__(self, user=None, **kwargs):
+    def __init__(self, user, **kwargs):
         self.user = user
-        super(AnswerForm, self).__init__(kwargs)
+        super().__init__(kwargs)
 
     def clean_question(self):
         question_qs = Question.objects.filter(id=self.cleaned_data["question"])
@@ -50,23 +48,6 @@ class AnswerForm(forms.Form):
         answer = Answer(**self.cleaned_data)
         answer.save()
         return answer
-
-
-class SignupForm(forms.Form):
-    username = forms.CharField(min_length=1)
-    email = forms.EmailField(required=False)
-    password = forms.CharField(min_length=1, widget=forms.PasswordInput)
-
-    def save(self):
-        user = User.objects.create_user(**self.cleaned_data)
-        user.save()
-        return user
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(min_length=1)
-    password = forms.CharField(min_length=1, widget=forms.PasswordInput)
-
 
 if __name__ == '__main__':
     pass
