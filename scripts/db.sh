f003084dea953ccc814a4478f9814e220aa83156
copybook48sheets <disabled1234@mail.ru>
2016-03-15 20:40:20 +0300

    1

--- scripts/db.sh
+++ scripts/db.sh
@@ -5,5 +5,3 @@ mysql -uroot -e "CREATE USER 'ask_user'@'localhost' IDENTIFIED BY 'ask_pwd';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON ask_db.* TO 'ask_user'@'localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
 
-cd ../ask
-python manage.py syncdb
