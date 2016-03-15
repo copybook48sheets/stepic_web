610b3ee0160f70425a37b0645bb1ccbcaf7872ca
copybook48sheets <disabled1234@mail.ru>
2016-03-15 18:23:11 +0300

    1
    1

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,18 +3,17 @@
 
 from django.conf.urls import url
 try:
-    from qa import views
+    from stepic_web.ask.qa import views
 except ImportError:
     import sys
     sys.path.append("/home/box")
-    from qa import views
+    from web.ask.qa import views
 
 
 urlpatterns = (url("^$", views.get_questions, name="first_page", kwargs={"question_type": "new"}),
-               url("^login/$",  views.login, name="login"),
-               url("^logout/$",  views.logout, name="logout"),
-               url("^signup/$",  views.signup, name="signup"),
-               url("^question/(?P<question_id>\d+)/$",  views.get_current_question, name="question"),
+               url("^login/$",  views.test, name="login"),
+               url("^signup/$",  views.test, name="signup"),
+               url("^question/(?P<id>\d+)/$",  views.get_current_question, name="question"),
                url("^ask/",  views.ask_question, name="ask"),
                url("^popular/$",  views.get_questions, name="popular", kwargs={"question_type": "popular"}),
                url("^new/$",  views.get_questions, name="new", kwargs={"question_type": "new"}))
