b100633d41dd9b734b6aa771bc68be5f25d88078
copybook48sheets <disabled1234@mail.ru>
2016-03-15 18:50:41 +0300

    1
    1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -53,7 +53,8 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request'
+    'django.core.context_processors.request',
+    'django.core.context_processors.csrf',
 )
 
 # for new django
