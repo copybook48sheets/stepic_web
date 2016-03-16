1c5cf0e10d94f526c80c5fb4d205665a810210a1
copybook48sheets <disabled1234@mail.ru>
2016-03-16 21:19:10 +0300

    1

--- hello.py
+++
@@ -1,6 +0,0 @@
-
-def application(environ, start_response):
-  qs = environ['QUERY_STRING']
-  ls = qs.split("&")
-  start_response('200 OK', [('Content-Type', 'text/plain')])
-  return [ "\n".join(ls) ]
