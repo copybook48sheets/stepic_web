fc47e417d0ef5276cb0f4583083725876bb57d0b
copybook48sheets <disabled1234@mail.ru>
2016-03-16 21:24:29 +0300

    1

--- db.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-sudo /etc/init.d/mysql restart
-mysql -uroot -e "CREATE DATABASE ask_db;"
-mysql -uroot -e "CREATE USER 'ask_user'@'localhost' IDENTIFIED BY 'ask_pwd';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ask_db.* TO 'ask_user'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-
